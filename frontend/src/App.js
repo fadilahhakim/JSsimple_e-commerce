5d749fec83dff5e20e68f0927baf7b13a363380c
Fadilah Hakim <moch.fadilahhakim12@gmail.com>
2022-10-21 18:11:32 +0700

    create header and footer component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello, react</h1>
-    </div>
+    
+    <>
+      <Header></Header>
+      <main>
+          <h1>welcome to dilshop</h1>
+      </main>
+      <Footer></Footer>
+    </>
+
   )
 }
 
