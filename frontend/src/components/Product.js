9819880b854e8614f933cafb390b628926c0dbfa
Fadilah Hakim <moch.fadilahhakim12@gmail.com>
2022-10-22 15:24:56 +0700

    update product, homepages and add dummy data

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,19 +1,25 @@
 import React from 'react'
 import {Card} from 'react-bootstrap'
 
-const Product = () => {
+const Product = ({product}) => {
   return (
    <Card className='my-3 p-3 rounded'>
-        <Card.Img></Card.Img>
+        <a href={`/product/${product._id}`}>
+        <Card.Img src={product.image}></Card.Img>
+        </a>
         <Card.Body>
+        <a href={`/product/${product._id}`}>
             <Card.Title as='div'>
-                <strong>Judul Product</strong>
+                <strong>{product.name}</strong>
             </Card.Title>
+        </a>
         </Card.Body>
         <Card.Text as='div'>
-        <div className='my-3'>Ratings</div>
+        <div className='my-3'>
+            {product.rating} from{product.numReviews} reviews
+        </div>
         </Card.Text>
-        <Card.Text as='h3'>Rp100.000,-</Card.Text>
+        <Card.Text as='h3'>Rp{product.price}</Card.Text>
    </Card>
   )
 }
--- frontend/src/pages/HomePages.js
+++ frontend/src/pages/HomePages.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import { Row , Col} from 'react-bootstrap'
 import Product from '../components/Product'
+import products from '../products'
 
 const HomePages = () => {
   return (
     <>
         <h1>Latest Products</h1>
         <Row>
-           <Col>
-              <Product></Product>
-           </Col>
+              {products.map((product)=>(
+                <Col sm={12} md={6} lg={4} xl={3}>
+                <Product product={product}></Product>
+                </Col>
+              ))}
         </Row>
     </>
   )
